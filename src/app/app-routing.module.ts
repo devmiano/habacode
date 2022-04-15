bdee1309f40ffa42704d3607e494bf6eaf8080b8
Miano <dev.miano@gmail.com>
2022-04-15 05:19:48 +0300

    Implemented routing for the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RepoComponent } from './components/repo/repo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HeroComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'repository', component: RepoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
